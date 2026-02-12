3c757b01c6236b127d621d227feab852409fccb1
Abenezer Tiglu <aintabeni@gmail.com>
2026-02-12 11:45:21 +0300

    Add basic functionality to calculate and display the sum of numbers from 1 to 10 and add .cpp suffix to the file

---
+++ LoopWithNoBody.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum = 0;    
+    // Getting the basic functionality
+    for (int x = 1; x <= 10; x++)
+    {
+        sum += x;
+    }
+    cout << sum;
+}
